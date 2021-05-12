a44b271ec2cfa172607756c6be2946c891ac22da
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-05-12 12:33:07 -0700

    Koa server instantialized

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const newrelic = require('newrelic');
 
-const fastify = require('fastify');
-const autoload = require('fastify-autoload');
-const path = require('path');
+const Koa = require('koa');
+const router = require('koa-router');
+const app = new Koa();
 
 const mgClient = require('./mongo/db');
 const pgPool = require('./postgres/db');
@@ -13,18 +13,9 @@ const npRoutes = './node-pg';
 
 const routes = npRoutes; // CHANGE THIS TO CHANGE DB
 
-const server = fastify({logger: false});
+const server = require(routes);
+app.use(server());
 
-server.register(autoload, {
-  dir: path.join(__dirname, routes)
-});
-
-// server.register(require('fastify-mongodb'), {
-//   // force to close the mongodb connection when app stopped
-//   // the default value is false
-
-//   url: 'mongodb://localhost:27017/sdc'
-// });
 if (routes === mgRoutes) {
 
   mgClient.connect('mongodb://localhost:27017/', (err) => {
@@ -34,26 +25,24 @@ if (routes === mgRoutes) {
     } else {
       console.log('Mongo connected');
 
-      server.listen(7763, (err) => {
-        if (err) {
-          server.log.error(err);
-          console.log(err);
+      try {
+        server.listen(7763);
+        console.log('listening on port 7763');
+      } catch {
+        console.log(err);
           process.exit(1);
-        }
-        server.log.info('Server Started');
-      })
+      }
     }
   });
 
 } else {
 
-  server.listen(7763, (err) => {
-    if (err) {
-      server.log.error(err);
-      console.log(err);
+  try {
+    server.listen(7763);
+    console.log('listening on port 7763');
+  } catch {
+    console.log(err);
       process.exit(1);
-    }
-    server.log.info('Server Started');
-  })
+  }
 
 }
