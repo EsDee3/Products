a8d4a717fe8224169f98cc19ea232fb80b39c12f
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-05-13 15:26:41 -0700

    Cleaned up files

--- index.js
+++ index.js
@@ -3,22 +3,15 @@ const newrelic = require('newrelic');
 const Koa = require('koa');
 const app = new Koa();
 
-const mgClient = require('./mongo/db');
 const pgPool = require('./postgres/db');
 
-const pgController = require('./postgres');
-const mgController = require('./mongo');
-const npController = require('./node-pg');
-
-const controller = mgController; // CHANGE THIS TO CHANGE DB
+const controller = require('./postgres');
 
 const router = require('@koa/router')();
 
 // ROUTES ///////////////////////////////////////
 router
-  .get('/pg/:pid', pgController.getProductObj)
-  .get('/mg/:pid', mgController.getProductObj)
-  .get('/np/:pid', npController.getProductObj)
+  .get('/:pid', controller.getProductObj)
   .get('/related/:pid', controller.getRelatedArray)
   .put('/cart', controller.updateSkuQty);
 
@@ -26,33 +19,11 @@ router
 app.use(router.routes());
 
 // CONNECT APP //////////////////////////////////
-if (controller === mgController) {
 
-  mgClient.connect('mongodb://localhost:27017/', (err) => {
-    if (err) {
-      console.log('Unable to connect to Mongo.');
-      process.exit(1);
-    } else {
-      console.log('Mongo connected');
-
-      try {
-        app.listen(7763);
-        console.log('listening on port 7763');
-      } catch {
-        console.log(err);
-          process.exit(1);
-      }
-    }
-  });
-
-} else {
-
-  try {
-    app.listen(7763);
-    console.log('listening on port 7763');
-  } catch {
-    console.log(err);
-      process.exit(1);
-  }
-
+try {
+  app.listen(7763);
+  console.log('listening on port 7763');
+} catch {
+  console.log(err);
+    process.exit(1);
 }
--- node-pg/db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-
-module.exports = new Pool({
-  database: 'sdc',
-  // max: 2000,
-  // idleTimeoutMillis: 0,
-  // connectionTimeoutMillis: 0
-})
