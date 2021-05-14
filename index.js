7627748d8830ff8ec8f496ad439ab152b72c2597
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-05-14 11:01:54 -0700

    Added authorization file for loader.io

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const newrelic = require('newrelic');
-const dotenv = require('dotenv').config();
 
 const Koa = require('koa');
 const app = new Koa();
 
 const pgPool = require('./postgres/db');
-
 const controller = require('./postgres');
 
+// ROUTES ///////////////////////////////////////
 const router = require('@koa/router')();
 
-// ROUTES ///////////////////////////////////////
 router
+  .get('/loaderio-3b303524c9bdca80e9e630b00467a94b', async (ctx) => {
+    ctx.body = 'loaderio-3b303524c9bdca80e9e630b00467a94b';
+  })
   .get('/:pid', controller.getProductObj)
   .get('/related/:pid', controller.getRelatedArray)
   .put('/cart', controller.updateSkuQty);
 
 
 app.use(router.routes());
-
 // CONNECT APP //////////////////////////////////
 
 try {
